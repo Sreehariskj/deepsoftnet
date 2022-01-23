e49ae91708019a5ac7a94bc2ae48158ba95f4432
Sreehariskj <sreehariskj@gmail.com>
2022-01-23 19:19:39 +0530

    connect backend login api with frontend login

--- frontend/src/screens/Login/Login.js
+++ frontend/src/screens/Login/Login.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
+import axios from 'axios'
 
 function Login() {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const submitHandler = async (e) =>{
+        e.preventDefault()
+        // console.log(email,password)
+
+        try {
+            const config = {
+                headers : {
+                    "Content-type":"application/json"
+                }
+            }
+            const {data} = await axios.post("/api/users/login",{
+                email,
+                password
+            },
+            config
+            )
+            console.log(data);
+            localStorage.setItem('userInfo',JSON.stringify(data))
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    }
 
     return (
         <div className='container'>
-         <form action=''>  
+         <form  onSubmit={submitHandler} action=''>  
             <div className='card'>
                 <h1>Login</h1>      
-                <input type='email' className='inputField' placeholder='Email' 
+                {error && <h4>{error} !</h4>}
+                <input type='email' className='inputField'
+                 placeholder='Email' value={email}
+                 onChange ={(e) => setEmail(e.target.value)} 
                  ></input>
-                <input type='password' className='inputField' placeholder='Password'
+                <input type='password' className='inputField'
+                 placeholder='Password' value={password}
+                 onChange ={(e) => setPassword(e.target.value)}
                  ></input>
                 <button type='submit' className='button'>Login</button>
             </div>
