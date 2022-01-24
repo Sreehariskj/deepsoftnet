7693ff0149fe4c37a4d9ff301e9b747404f5a441
Sreehariskj <sreehariskj@gmail.com>
2022-01-24 11:08:16 +0530

    fixed issue of redirect

--- frontend/src/screens/Login/Login.js
+++ frontend/src/screens/Login/Login.js
@@ -1,58 +1,69 @@
-import React, { useState } from 'react';
-import '../common.css'
-import axios from 'axios'
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "../common.css";
+import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // console.log(email,password)
 
-    const submitHandler = async (e) =>{
-        e.preventDefault()
-        // console.log(email,password)
-        navigate('/')
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
 
-        try {
-            const config = {
-                headers : {
-                    "Content-type":"application/json"
-                }
-            }
-            const {data} = await axios.post("/api/users/login",{
-                email,
-                password
-            },
-            config
-            )
-            console.log(data);
-            localStorage.setItem('userInfo',JSON.stringify(data))
-        } catch (error) {
-            setError(error.response.data.message);
-        }
+      // @redirect to dashboard after sucess login
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data.message);
     }
+  };
 
-    return (
-        <div className='container'>
-         <form  onSubmit={submitHandler} action=''>  
-            <div className='card'>
-                <h1>Login</h1>      
-                {error && <h4>{error} !</h4>}
-                <input type='email' className='inputField'
-                 placeholder='Email' value={email}
-                 onChange ={(e) => setEmail(e.target.value)} 
-                 ></input>
-                <input type='password' className='inputField'
-                 placeholder='Password' value={password}
-                 onChange ={(e) => setPassword(e.target.value)}
-                 ></input>
-                <button type='submit' className='button'>Login</button>
-            </div>
-         </form>
+  return (
+    <div className="container">
+      <form onSubmit={submitHandler} action="">
+        <div className="card">
+          <h1>Login</h1>
+          {error && <h4>{error} !</h4>}
+          <input
+            type="email"
+            className="inputField"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            className="inputField"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit" className="button">
+            Login
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
